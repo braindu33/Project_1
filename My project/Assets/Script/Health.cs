fd1fe120aa9665c075d82857b40bc3e4f7877224
matmat1421 <mateomichel73@gmail.com>
2023-10-22 20:34:29 +0200

    harvestable function

--- My project/Assets/Script/Health.cs
+++ My project/Assets/Script/Health.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 5;
+    [SerializeField] private UnityEvent healthDecreased;
+    [SerializeField] private UnityEvent healthReachedMin;
+    
     private int _health;
     void Awake()
     {
@@ -14,10 +18,11 @@ public class Health : MonoBehaviour
     public void Decrease(int damage)
     {
         _health -= damage;
+        healthDecreased.Invoke();
 
         if (_health <= 0)
         {
-            Destroy(gameObject);
+            healthReachedMin.Invoke();
         }
     }
 }
