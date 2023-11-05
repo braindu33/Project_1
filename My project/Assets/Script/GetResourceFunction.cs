ad29b4d6d9be1911642ae77accd081926aefc491
DESKTOP-JTLGJDD\pc <rusimovk@yahoo.com>
2023-11-05 20:59:31 +0100

    Bonne config de la recolte Ajout de ma propre scène (a na pas toucher svp)

--- My project/Assets/Script/GetResourceFunction.cs
+++ My project/Assets/Script/GetResourceFunction.cs
@@ -8,15 +8,26 @@ public class GetResourceFunction : MonoBehaviour
     [SerializeField] private int value;
     [SerializeField] private int index;
 
-    
+
     public void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Inventory.Instance.GetResource(index).AddResource(value);
-            Destroy(gameObject);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+                GetResourceFunction getResourceFunction = hit.collider.GetComponent<GetResourceFunction>();
+
+                if (getResourceFunction == this)
+                {
+                    Inventory.Instance.GetResource(index).AddResource(value);
+                    Destroy(gameObject);
         
-            Debug.Log("The resource is take");
+                    Debug.Log("The resource is take");
+                }
+            }
         }
     }
 }
