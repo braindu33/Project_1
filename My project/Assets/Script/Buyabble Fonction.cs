0b09325da3131d10457027444b6aa56795cda8d5
ZZKonstan <129211551+ZZKonstan@users.noreply.github.com>
2024-03-04 12:03:29 +0100

    Update Buyabble Fonction.cs

--- My project/Assets/Script/Buyabble Fonction.cs
+++ My project/Assets/Script/Buyabble Fonction.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 public class BuyabbleFonction : MonoBehaviour
 {
     //public List<Resource> PriceAndIndex = new();
+    [FormerlySerializedAs("itemBuy")] public List<ItemBuy> itemBuys = new ();
     
     [SerializeField] private int itemPrice = 1;
     [SerializeField] private int itemIndex = 0;
@@ -14,9 +15,15 @@ public class BuyabbleFonction : MonoBehaviour
     [SerializeField] private Vector3 positionSpawn;
 
     private Spawner _spawner;
+    
 
     private void Awake()
     {
+        foreach(var itemBuy in itemBuys)
+        {
+            itemBuy.buyButton.onClick.AddListener(itemBuy.Buy)
+        }
+        
         _spawner = GetComponent<Spawner>();
     }
 
@@ -27,7 +34,7 @@ public class BuyabbleFonction : MonoBehaviour
 
     public void Buy()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, 3))
@@ -37,7 +44,26 @@ public class BuyabbleFonction : MonoBehaviour
             {
                 _spawner.Spawn(positionSpawn);
             }
+        }*/
+
+        if (Inventory.Instance.RemoveResource(itemIndex, itemPrice))
+        {
+            _spawner.Spawn(positionSpawn);
         }
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    
+    private void Close()
+    {
+        gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     
 }
