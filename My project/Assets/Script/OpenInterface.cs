6bd4bb291f60835c92e291e38e57fa70b0efa8d8
ZZKonstan <129211551+ZZKonstan@users.noreply.github.com>
2024-03-04 12:08:25 +0100

    Update OpenInterface.cs

--- My project/Assets/Script/OpenInterface.cs
+++ My project/Assets/Script/OpenInterface.cs
@@ -5,32 +5,15 @@ using UnityEngine.Events;
 public class OpenInterface : MonoBehaviour
 {
     [SerializeField] private int maxDistance;
-
-    //private Vector3 playerPos;
-
+    
     [SerializeField] private OpenInterface open;
     [SerializeField] private CraftFonction craft;
     private RaycastHit hit;
 
-    /*private void Awake()
-    {
-        playerPos = playerTreansform.position;
-    }*/
-
     public void OnTriggerEnter(Collider other)
-    {
-        /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, maxDistance))
-        {
-            if(maxDistance >= )
-        }*/
-        Debug.Log("First");
-        
+    {        
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Second");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit, maxDistance))
